eca92188f9277a68639837eb77877344bf77698a
Kevin <kevin.06.huang@gmail.com>
2023-07-26 00:42:59 -0700

    Got typescript frontend portion working

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- frontend_typescript/src/pages/home.page.tsx
+++ frontend_typescript/src/pages/home.page.tsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+
 const HomePage = () => {
+  useEffect(() => {
+    let res;
+    async function fetchData() {
+      res = fetch("http://127.0.0.1:8000/", {
+        method: "GET",
+      });
+      console.log(await res);
+    }
+    fetchData();
+  }, []);
+  
+  const fetchData = async () => {
+    const res = fetch("http://127.0.0.1:8000/", {
+      method: "GET",
+    });
+    console.log(await res);
+  };
+
   return (
     <>
       <section className="bg-ct-blue-600 min-h-screen pt-20">
@@ -6,6 +26,9 @@ const HomePage = () => {
           <p className="text-3xl font-semibold">
             This is the typescript portion of the monorepo
           </p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={fetchData}>
+            Fetch
+          </button>
         </div>
       </section>
     </>
