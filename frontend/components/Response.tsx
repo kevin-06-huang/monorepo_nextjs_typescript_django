1a7eab5d3a53df03f7868bca7acb8435c367c5a5
Kevin <kevin.06.huang@gmail.com>
2023-07-25 23:45:42 -0700

    Fetching successfully; need to fix cors on django

---
+++ frontend/components/Response.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useEffect } from "react";
+
+const Response = () => {
+  useEffect(() => {
+    let res;
+    async function fetchData() {
+      res = fetch("http://127.0.0.1:8000/", {
+        method: "GET",
+      });
+      console.log(await res);
+    }
+    fetchData();
+  }, []);
+  
+
+  return(
+    <div>Test</div>
+  );
+};
+
+export default Response;
