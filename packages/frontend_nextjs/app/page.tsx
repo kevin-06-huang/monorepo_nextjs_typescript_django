594d999c47b788d9d00726e97db1d61367015dc7
Kevin <kevin.06.huang@gmail.com>
2023-07-30 17:13:44 -0700

    Add use client to external component library instead

--- packages/frontend_nextjs/app/page.tsx
+++ packages/frontend_nextjs/app/page.tsx
@@ -1,4 +1,4 @@
-import ClientComponentResponseWrapper from "@/components/ClientComponentResponseWrapper";
+import Response from "../../common/components/src/Response";
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
           <p className="text-3xl font-semibold">SSR HTML</p>
         </div>
-        <ClientComponentResponseWrapper />
+        <Response />
       </section>
     </main>
   );
